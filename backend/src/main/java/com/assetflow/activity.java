70da22e577cfae433138fb7737c55e6c16f96ec7
Vijay D <advijay116@gmail.com>
2026-07-12 14:50:05 +0530

    Add Activity class with message property

---
+++ backend/src/main/java/com/assetflow/activity.java
@@ -0,0 +1,17 @@
+package com.assetflow.model;
+
+public class Activity {
+
+    private String message;
+
+    public Activity() {
+    }
+
+    public Activity(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
